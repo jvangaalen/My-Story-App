7d98dd33f39428bb1af876ea61ec866790077c6c
jvangaalen <jerome.vangaalen@gmail.com>
2018-02-19 20:04:25 -0500

    Changed button field in html to form fields and added code to link to javascript. Added Java script to take input fields and print them into the story.

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
-// make a form with input fields for Name, noun, verb, adverb etc..
-// create a submit button after each form field input is filled out
+
+// create a variable that will add id's from the word-form class  into  shortstory after submit button is clicked.
+validate = function() {
+    var noun = document.getElementById("noun").value;
+    var name = document.getElementById("name").value;
+    var place = document.getElementById("place").value;
+    var animal = document.getElementById("animal").value;
+    var verb = document.getElementById("verb").value;
 // add alert prompt if not all fields are filled in saying, "Oops, please fill in blank entries"
+//    function user-word(evt:any) { 
+   // evt.preventDefault(); 
 // stop js from loading unless all form fields are filled in
 // clear form after entry submission
 // add form field entry inputs to story line in .story
-// load .story to page. 
-
-
-
-
-
-
-
-const noun = prompt ("Please enter a noun");
-const name = prompt ("Please enter a name");
-const place = prompt("Please enter a place");
-const animal = prompt ("Please enter an animal");
-const verb = prompt ("Please enter a verb");
-
-let shortStory = "This is the story of " + name; + " the " + animal;
+let shortStory = "This is the story of " + name  + " the " + animal;
 shortStory += ". He likes to " + verb + " on Sundays. ";
-shortStory += " He lives in a  " + noun + " and realy likes to visit " + place + " Where he has a summer house. "
+ shortStory += " He lives in a  " + noun + " and realy likes to visit " + place + " Where he has a summer house. ";
+// load .story to page. 
+document.write("<p>" + shortStory + "</p>");
+};
 
-document.write("<p>" + shortStory + "</p>") in document.createElement(story);
+
+
+
+
+
+// const noun = prompt ("Please enter a noun");
+// const name = prompt ("Please enter a name");
+// const place = prompt("Please enter a place");
+// const animal = prompt ("Please enter an animal");
+// const verb = prompt ("Please enter a verb");
+
+// let shortStory = "This is the story of " + name; + " the " + animal;
+// shortStory += ". He likes to " + verb + " on Sundays. ";
+// shortStory += " He lives in a  " + noun + " and realy likes to visit " + place + " Where he has a summer house. "
+
+// document.write("<p>" + shortStory + "</p>") in document.createElement(story);
