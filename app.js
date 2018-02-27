12215cddc1ae31c2bfe55c5f26332d6f79227467
jvangaalen <jerome.vangaalen@gmail.com>
2018-02-26 20:37:14 -0500

    Added javaScript to write to shortStory achanged a class in Html to help

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 
 // create a variable that will add id's from the word-form class  into  shortstory after submit button is clicked.
-validate = function() {
+var shortStory;
+var test = function validate (event) {
     var noun = document.getElementById("noun").value;
+    console.log(noun);
     var name = document.getElementById("name").value;
     var place = document.getElementById("place").value;
     var animal = document.getElementById("animal").value;
     var verb = document.getElementById("verb").value;
 // add alert prompt if not all fields are filled in saying, "Oops, please fill in blank entries"
-//    function user-word(evt:any) { 
-   // evt.preventDefault(); 
+   
 // stop js from loading unless all form fields are filled in
+    //document.getElementById("word-form").addEventListener("click", function(event){
+    //event.preventDefault()
+//});
 // clear form after entry submission
 // add form field entry inputs to story line in .story
-let shortStory = "This is the story of " + name  + " the " + animal;
+ shortStory = "This is the story of " + name  + " the " + animal;
 shortStory += ". He likes to " + verb + " on Sundays. ";
  shortStory += " He lives in a  " + noun + " and realy likes to visit " + place + " Where he has a summer house. ";
 // load .story to page. 
-document.write("<p>" + shortStory + "</p>");
-};
+//document.write("<p>" + shortStory + "</p>");
+
+console.log(shortStory);
+return shortStory;
+}
+var writeStory=document.querySelector("#story");
+writeStory.innerHTML= "<p>" + test + "</p>";
 
 
-
+console.log(shortStory);
 
 
 
