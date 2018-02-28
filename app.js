5d4202007e771b91a61a73bc4e45e24eac4970a7
jvangaalen <jerome.vangaalen@gmail.com>
2018-02-27 22:26:30 -0500

    Made changes to HTML form element adding return to the validate statement inside it. Moved writeStory and innerHTML, return false inside my function. made the event listener work by changing the class word form in html to an id. added some style to the label element in css

--- app.js
+++ app.js
@@ -1,46 +1,38 @@
 
 // create a variable that will add id's from the word-form class  into  shortstory after submit button is clicked.
-var shortStory;
-var test = function validate (event) {
+function validate (event) {
     var noun = document.getElementById("noun").value;
-    console.log(noun);
     var name = document.getElementById("name").value;
     var place = document.getElementById("place").value;
     var animal = document.getElementById("animal").value;
     var verb = document.getElementById("verb").value;
+    
 // add alert prompt if not all fields are filled in saying, "Oops, please fill in blank entries"
    
 // stop js from loading unless all form fields are filled in
-    //document.getElementById("word-form").addEventListener("click", function(event){
-    //event.preventDefault()
-//});
+    document.getElementById("word-form").addEventListener("click", function(event){
+    event.preventDefault()
+});
 // clear form after entry submission
 // add form field entry inputs to story line in .story
- shortStory = "This is the story of " + name  + " the " + animal;
+var shortStory = "This is the story of " + name  + " the " + animal;
 shortStory += ". He likes to " + verb + " on Sundays. ";
- shortStory += " He lives in a  " + noun + " and realy likes to visit " + place + " Where he has a summer house. ";
+shortStory+= " He lives in a  " + noun + " and realy likes to visit " + place + " Where he has a summer house. ";
 // load .story to page. 
 //document.write("<p>" + shortStory + "</p>");
-
-console.log(shortStory);
-return shortStory;
-}
+//console.log(shortStory);
 var writeStory=document.querySelector("#story");
-writeStory.innerHTML= "<p>" + test + "</p>";
+writeStory.innerHTML= "<p>" + shortStory + "</p>";
+return false;
+}
+
+/* Becky's comments:
+So close! Here's what I realized we hadn't done quite yet, and this is how I fixed it:
+- in index.html line 24, change onsubmit="validate()" to onsubmit="return validate();". This tells the DOM that we want to grab the output of the function validate() and keep it in the DOM.
+- in app.js, I: 
+    - removed the var shortStory; declaration on line 3, and added it to the beginning of line 18. I also just removed all the shortStory += lines, but you don't have to if you don't want to!
+    - I moved lines 28 & 29 (writeStory declaration and innerHTML setting) and moved them inside of the function again.
+    - removed the console.log(shortStory) on line 32 since it's out of scope.
+*/
 
 
-console.log(shortStory);
-
-
-
-// const noun = prompt ("Please enter a noun");
-// const name = prompt ("Please enter a name");
-// const place = prompt("Please enter a place");
-// const animal = prompt ("Please enter an animal");
-// const verb = prompt ("Please enter a verb");
-
-// let shortStory = "This is the story of " + name; + " the " + animal;
-// shortStory += ". He likes to " + verb + " on Sundays. ";
-// shortStory += " He lives in a  " + noun + " and realy likes to visit " + place + " Where he has a summer house. "
-
-// document.write("<p>" + shortStory + "</p>") in document.createElement(story);
